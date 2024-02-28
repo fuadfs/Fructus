cdb0268dda83400e7731a8d1198774ea7e3bd186
Fuad Fadlila Surenggana <35410577+fuadfs@users.noreply.github.com>
2024-02-28 09:04:31 +0800

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Fuad Fadlila Surenggana on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
